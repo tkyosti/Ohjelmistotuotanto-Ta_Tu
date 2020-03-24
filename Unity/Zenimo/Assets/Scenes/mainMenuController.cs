268fa457bcb53cc93d23c841f5ed3813474737ed
hiltar <tartsihiltsu@gmail.com>
2020-03-25 00:05:25 +0200

    main menu koodi nappeihin

---
+++ Unity/Zenimo/Assets/Scenes/mainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuController : MonoBehaviour
+{
+    // Load the game scene
+    public void playGame() {
+        SceneManager.LoadScene("gameBoardScene");
+    }
+    // Open the information of game
+    public void info() {
+        
+    }
+    // Shut the game
+    public void exitGame() {
+        Application.Quit();
+    }
+}
